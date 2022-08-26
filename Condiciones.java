47cb457eef8700934ffb583840e4176d7ff62c24
pacoalhe <pacoalhe@hotmail.com>
2022-08-25 19:43:54 -0500

    condicionesincluyenoaprobado_duplicadonoaprobado

--- Condiciones.java
+++ Condiciones.java
@@ -13,18 +13,5 @@ public class Condiciones{
    } else {
      System.out.println("El alumno reprobó con: " + promedio);
    }
-
-   int matematicas = 5;
-   int biologia    = 5;
-   int quimica     = 5;
-   int promedio    = 0;
-
-   promedio = (matematicas + biologia + quimica) / 3;
-
-   if(promedio >= 6){
-    System.out.println("El alumno aprobó con: " + promedio);
-   } else {
-     System.out.println("El alumno reprobó con: " + promedio);
-   }
  }
 }
